a045493f5a292c182598cc5b4ec4f4e152c2f539
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-09 16:23:13 +0000

    task 0 crated documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-""""A type-annotated function add that takes a float a and 
+""""A type-annotated function add that takes a float a and
 a float b as arguments and returns their sum as a float.
 """
 
+
 def add(a: float, b: float) -> float:
+    """Method that add two floats and return one float"""
     return (a + b)
