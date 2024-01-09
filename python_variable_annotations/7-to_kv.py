c122fcbddfd8e4272e6d71d6f3b99ee5c63419a1
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-09 23:39:34 +0000

    task 7 v2 corrected tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,4 +9,4 @@ from typing import List, Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Method that takes a string k and an int OR float
         v as arguments and returns a tuple."""
-    return [k, v*v]
+    return (k, v*v)
