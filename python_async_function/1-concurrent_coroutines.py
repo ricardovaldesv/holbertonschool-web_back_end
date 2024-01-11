fa3f0d5a849e33c0329c2da9c895eea5fc4a759e
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-11 22:49:07 +0000

    task 1 v0

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def wait_n(n: int, max_delay: float) -> list[float]:
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
+
+
