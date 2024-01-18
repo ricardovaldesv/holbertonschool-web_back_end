df9041612376a55c5069bdd60388d0661b8563eb
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-18 09:52:16 -0500

    task 2 v2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -62,12 +62,32 @@ class Server:
         return index_r
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Method that takes the same arguments (and defaults) as get_page
+        and returns a dictionary containing the following key-value pairs:
+
+            - page_size: the length of the returned dataset page
+            - page: the current page number
+            - data: the dataset page (equivalent to return from previous task)
+            - next_page: number of the next page, None if no next page
+            - prev_page: number of the previous page, None if no previous page
+            - total_pages: the total number of pages in the dataset
+              as an integer
+        """
         list_data = self.get_page(page, page_size)
         dic = {}
         dic['page_size'] = page_size
         dic['page'] = page
         dic['data'] = list_data
-        dic['next_page'] = page + 1
-        dic['prev_page'] = page - 1
+        if page == 0 or page == 1:
+            dic['next_page'] = None
+        else:
+            dic['next_page'] = page + 1
+
+        if page == 0 or page == 1:
+            dic['prev_page'] = None
+        else:
+            dic['prev_page'] = page - 1
+
         dic['total_pages'] = int(len(self.dataset()) / page_size)
         return dic
