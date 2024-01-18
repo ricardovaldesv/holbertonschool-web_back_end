b6a3a0aaca6cf7c992684ee9237b45f00128bd57
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-18 14:09:33 -0500

    task 3 v4

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -28,7 +28,7 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> dict[int, List]:
+    def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
@@ -66,3 +66,52 @@ class Server:
             "data": [item for item in data_page if item is not None]
         }
         return hyper_info
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        Retrieve a specific page of data from a CSV file.
+        '''
+        assert type(page) is int and type(page_size) is int, "Must be int"
+        assert page > 0 and page_size > 0, "The page and page_size must be > 0"
+        list_of_data = []
+        with open(self.DATA_FILE) as f:
+            reader = csv.reader(f)
+            indexes = index_range(page, page_size)
+            for num, line in enumerate(reader):
+                if num in range(indexes[0], indexes[1]):
+                    list_of_data.append(line)
+        return list_of_data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''method that takes the same arguments (and defaults)as get_page
+        and returns a dictionary containing the following key-value pairs:
+        - page_size: the length of the returned dataset page
+        - page: the current page number
+        - data: the dataset page (equivalent to return from previous task)
+        - next_page: number of the next page, None if no next page
+        - prev_page: number of the previous page, None if no previous page
+        - total_pages: the total number of pages in the dataset as an integer
+        '''
+        dict_of_data = {}
+        indexes = index_range(page, page_size)
+        if (indexes[0] + 1) > len(self.dataset()):
+            dict_of_data["page_size"] = 0
+        else:
+            dict_of_data["page_size"] = indexes[1] - indexes[0]
+        dict_of_data["page"] = page
+        dict_of_data["data"] = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        dict_of_data["next_page"] = page + 1 if page < total_pages else None
+        dict_of_data["prev_page"] = page - 1 if page > 1 else None
+        dict_of_data["total_pages"] = total_pages
+        return dict_of_data
+
+
+def index_range(page, page_size):
+    """
+    Calculate the start and end index for a given page and page_size.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return tuple([start_index, end_index])
