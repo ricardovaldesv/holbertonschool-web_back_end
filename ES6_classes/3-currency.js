982bffff7a53d39cfba94f0f4026b9b2e52f8161
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-19 22:04:04 -0500

    task 3 V1

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = "";
-    this._name = "";
+    this._code = '';
+    this._name = '';
 
     this.code = code;
     this.name = name;
@@ -12,8 +12,8 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("Code must be a string");
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
@@ -23,8 +23,8 @@ export default class Currency {
   }
 
   set name(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
