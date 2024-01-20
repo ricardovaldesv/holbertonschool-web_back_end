272d74694c9e32c0c7165d3446795f09e6c0b6e0
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-19 19:55:05 -0500

    task 1 v8

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let newtask = true;
-    let newtask2 = false;
+    const newtask = true;
+    const newtask2 = false;
   }
 
   return [task, task2];
