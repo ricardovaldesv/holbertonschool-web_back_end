4f62bc86e26cb8c789c5cfc0aaf644af508a43e8
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-19 21:16:24 -0500

    task 10 v2

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,6 +2,6 @@ export default function appendToEachArrayValue(array, appendString) {
   for (let i = 0; i < array.length; i++) {
     array[i] = appendString + array[i];
   }
-
+  
   return array;
 }
