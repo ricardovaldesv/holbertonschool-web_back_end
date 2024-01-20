1452b2b0f4d4478dd0575871ce782ecb956dad63
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-19 21:47:21 -0500

    task 2 V1

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,6 @@
-
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = "";
+    this._name = '';
     this._length = 0;
     this._students = [];
 
@@ -15,19 +14,19 @@ export default class HolbertonCourse {
   }
 
   set name(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
-  
+
   get length() {
     return this._length;
   }
-  
+
   set length(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("Length must be a number");
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
@@ -37,10 +36,9 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(value) || !value.every((item) => typeof item === "string")) {
-      throw new TypeError("Students must be an array of strings");
+    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
   }
 }
-  
