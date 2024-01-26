e8bcd4c0c6ca6c396bbf7232514dc104cfa92590
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-26 16:33:46 -0500

    task 2 v1

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,18 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (false) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error(''));
-    }
-  });
+  return promise
+    .then((result) => {
+      console.log("Got a response from the API");
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.error("Got an error from the API");
+      return new Error();
+    });
 }
-
-handleResponseFromAPI()
-  .then((result) => {
-    console.log("Got a response from the API");
-    return result;
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
