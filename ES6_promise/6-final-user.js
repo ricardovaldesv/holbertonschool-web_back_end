861ec68af3e001c6ea4c2f9882a7069d4664f060
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-26 18:38:03 -0500

    task 6 v0

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      return [];
+    });
+}
