82962d6da53f5379fce5f97ae0e6bdd11cd03f8d
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-26 19:10:16 -0500

    task 6 v6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPhotoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
-    .then((results) => (results))
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled'
+        ? result.value
+        : result.reason,
+    })))
     .catch((error) => {
       console.error(error);
       return [];
