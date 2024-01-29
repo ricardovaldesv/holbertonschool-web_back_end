a0ba2b82a8957806d00315523ec9f6fac65704e0
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-29 15:43:41 -0500

    task 8 V1

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (numerator && denominator) {
     return numerator / denominator;
-  }
-  return new Error('cannot divide by 0');
+  } 
+  throw Error('cannot divide by 0');
 }
