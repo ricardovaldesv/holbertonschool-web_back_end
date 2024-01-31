bf2f12d13a37c9cab19587e114877bc30bb21f1b
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-31 16:53:20 -0500

    task 1 V1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,10 @@
 export default function getListStudentIds(miArray) {
   const newArray = [];
+
   if (Array.isArray(miArray)) {
-    for (let i = 0; i < miArray.length; i++) {
-      newArray.push(miArray[i].id);
-    }
+    const newArray = miArray.map((valor) => valor.id);
+    return newArray;
   }
+
   return newArray;
 }
