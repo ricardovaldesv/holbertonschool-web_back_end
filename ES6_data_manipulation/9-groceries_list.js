77c42b18bc2aeddab9f1871d362ddffa2354d521
ricardovaldesv <ricardovaldes@icloud.com>
2024-02-01 17:24:35 -0500

    tak 9 V0

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const miMapa = new Map();
+  miMapa.set('Apples', 10);
+  miMapa.set('Tomatoes', 10);
+  miMapa.set('Pasta', 1);
+  miMapa.set('Rice', 1);
+  miMapa.set('Banana', 5);
+  return miMapa;
+}
