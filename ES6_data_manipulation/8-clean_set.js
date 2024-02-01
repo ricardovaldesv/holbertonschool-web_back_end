eeccad0a1086c4fa5a6fc02a17dd75000aa71e4b
ricardovaldesv <ricardovaldes@icloud.com>
2024-02-01 16:48:46 -0500

    tak 8 V0

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const iterator = set.values();
+  let strResult = '';
+  for (let i = 0; i < set.size; i += 1) {
+    const str1 = iterator.next().value;
+    if (str1.startsWith(startString)) {
+      const longitudPrefijo = startString.length;
+      const resultado = str1.substring(longitudPrefijo);
+      strResult += `${resultado}-`;
+    }
+  }
+  const strSinGuion = strResult.slice(0, -1);
+  return strSinGuion;
+}
